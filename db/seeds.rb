a6cf47db80850dcd07a9523a8a186407abf0a46c
surielisrodriguez <surielisrodriguez7@gmail.com>
2023-06-13 10:46:57 -0700

    Added Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
 user2 = User.where(email: "test2@example.com").first_or_create(password: "password", password_confirmation: "password")
 
-apartments=[
+ca_apartments = [
     {
         street:"Andros Pl",
         unit:"1",
@@ -11,18 +11,6 @@ apartments=[
         price:"2,500",
         bedrooms:2,
         bathrooms:2,
-        pets:"yes",
-        image:"../assets/images/apartment.jpeg"
-    },
-    {
-        street:"Grove Ave",
-        unit:"833",
-        city:"Altamonte Springs",
-        state:"FL",
-        square_footage:1500,
-        price:"1,500",
-        bedrooms:3,
-        bathrooms:3.5,
         pets:"yes",
         image:"../assets/images/apartment.jpeg"
     },
@@ -39,3 +27,26 @@ apartments=[
         image:"../assets/images/apartment.jpeg"
     }
 ]
+fl_apartments = [
+    {
+        street:"Grove Ave",
+        unit:"833",
+        city:"Altamonte Springs",
+        state:"FL",
+        square_footage:1500,
+        price:"1,500",
+        bedrooms:3,
+        bathrooms:3.5,
+        pets:"yes",
+        image:"../assets/images/apartment.jpeg"
+    }
+]
+
+  fl_apartments.each do |apartment|
+    user1.apartments.create(apartment)
+    p "creating: #{apartment}"
+  end
+  ca_apartments.each do |apartment|
+    user2.apartments.create(apartment)
+    p "creating: #{apartment}"
+  end
