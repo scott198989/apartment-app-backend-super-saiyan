57afbc51fb63a63e5c26b832be1360157b14aa3a
surielisrodriguez <surielisrodriguez7@gmail.com>
2023-06-13 17:01:36 -0700

    Added more testing to the apartments spec file and updated the apartments controller

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -3,4 +3,17 @@ class ApartmentsController < ApplicationController
         apartments = Apartment.all 
         render json: apartments
     end
+
+    def create 
+        apartments = Apartment.create(apt_params)
+        if apartment.valid?
+            render json: apartment
+        else
+            render json: apartment.errors, status: 422
+        end
+    end
+
+    private
+    def apt_params
+        params.require(:apartment).permit(:street, :city, :state, :manager, :email, :price, :bedrooms, :bathrooms, :pets, :image, :user_id)
 end
--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -28,5 +28,57 @@ RSpec.describe "Apartments", type: :request do
       expect(apartment.first['street']).to eq('Andros Pl')
     end
   end
+  describe "POST /create" do
+    it "creates an apartment listing" do
+      apt_params = {
+        apartment: {
+          street:"Andros Pl",
+          unit:"1",
+          city:"San Diego",
+          state:"CA",
+          square_footage:1200,
+          price:"2,500",
+          bedrooms:2,
+          bathrooms:2,
+          pets:"yes",
+          image:"../assets/images/apartment.jpeg",
+          user_id: user.id
+        }
+      }
+  
+      post '/apartments', params: apt_params
+      expect(response.status).to eq(200) # Expect a 200 OK status
+      json = JSON.parse(response.body)
+      expect(json['street'])..to include("can't be blank")
+    end
+
+    # Validates that an apartment will not be created without a valid street name
+    it 'will not create an apartment listing without a street' do
+      apt_params = {
+        apartment: {
+          street:nil,
+          unit:"1",
+          city:"San Diego",
+          state:"CA",
+          square_footage:1200,
+          price:"2,500",
+          bedrooms:2,
+          bathrooms:2,
+          pets:"yes",
+          image:"../assets/images/apartment.jpeg",
+          user_id: user.id
+        }
+      }
+      #request
+      post '/apartment', params: apt_params
+      # assertion on the response
+      # status
+      p 'create response', response.status
+      expect(response.status).to eq(422)
+      # payload
+      apartment_json = JSON.parse(response.body)
+      expect(apartment_json['name']).to include "can't be blank"
+    end
+  
 end
 
